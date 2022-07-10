b4f6118cd71ca62deb65a89caff56126c8b6604a
Sina <sinasteyn@gmail.com>
2022-07-11 02:14:13 +0430

    initial commit

---
+++ wc-sync-with-3p-API.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Plugin Name: WC sync with third party API 
+ * Version: 1.0.0
+ * Description: This plugin synchronise woocommerce with a third party API data
+ * Author: Sina Ghiasi
+ * Text Domain: wc-sync-with-API
+ *
+ * @author Sina Ghiasi
+ */
+
+if (!defined('ABSPATH')) {
+    exit;
+}
